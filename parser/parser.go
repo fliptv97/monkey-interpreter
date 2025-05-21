f7477c5ec992af672aea078e74f4f9c6c5f7e0ca
Philip Kazakov <fliptv97@gmail.com>
2025-05-21 21:14:24 +0300

    Add incomplete AST implementation, which currently supports only `let` statements
    We're now able to successfully parse `let` statements, except expressions part of it.

---
+++ parser/parser.go
@@ -0,0 +1,102 @@
+package parser
+
+import (
+	"fmt"
+	"github.com/fliptv97/monkey-interpreter/ast"
+	"github.com/fliptv97/monkey-interpreter/lexer"
+	"github.com/fliptv97/monkey-interpreter/token"
+)
+
+type Parser struct {
+	lexer     *lexer.Lexer
+	currToken token.Token
+	peekToken token.Token
+	errors    []string
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{lexer: l, errors: []string{}}
+
+	/* These two calls initialise `p.currToken` and `p.peekToken`
+	 * with the first and second tokens respectively */
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.lexer.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for !p.currTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() ast.Statement {
+	stmt := &ast.LetStatement{Token: p.currToken}
+
+	if !p.consumeSpecific(token.IDENT) {
+		return nil
+	}
+	stmt.Name = &ast.Identifier{
+		Token: p.currToken,
+		Value: p.currToken.Literal,
+	}
+
+	if !p.consumeSpecific(token.ASSIGN) {
+		return nil
+	}
+	for !p.currTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) currTokenIs(t token.TokenType) bool {
+	return p.currToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) consumeSpecific(t token.TokenType) bool {
+	if !p.peekTokenIs(t) {
+		p.peekError(t)
+		return false
+	}
+	p.nextToken()
+	return true
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
